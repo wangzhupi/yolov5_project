e6b8f244443b7febd43901d63c267344daf92bac
pengyuyan <50665088+wangzhupi@users.noreply.github.com>
2023-03-30 18:00:18 +0800

    将新得到的yaml文件放入train中进行训练

--- train.py
+++ train.py
@@ -435,7 +435,9 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default=ROOT / 'yolov5s.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
+#     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
+    # 将新得到的yaml文件放入其中
+    parser.add_argument('--cfg', type=str, default=ROOT/'models/yolov5s_c2f.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=100, help='total training epochs')
