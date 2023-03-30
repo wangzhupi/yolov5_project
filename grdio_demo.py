ae1ac89d9063e949f517f07da0409b3f0aa0d309
pengyuyan <50665088+wangzhupi@users.noreply.github.com>
2023-03-30 12:11:05 +0800

    update一下

--- grdio_demo.py
+++ grdio_demo.py
@@ -1,16 +1,17 @@
 import torch
 import gradio as gr
 
-# 模型加载
-# 这个模型加载的函数是利用torch的函数来进行的
-# 后面搜一下这些参数的含义以及用法
-model = torch.hub.load("./","custom",path = "path/to/weights",source="local)
- 
-# 增加网页描述信息
+
+# 加载模型
+# torch.hub.load函数参数及用法
+model = torch.hub.load("./","custom",path = "/home/udf/workspace/wangzijian/yolov5/yolov5s.pt",source= "local")
+
+
+# 增加描述
 title = "基于Yolov5的演示项目"
+
 desc = "基于Yolov5的演示项目非常简洁和方便"
 
-# 检测函数传入基本的conf iou 以及img
 def det_image(img,conf_thres,iou_thres):
     model.conf = conf_thres 
     model.iou = iou_thres 
@@ -22,11 +23,13 @@ base_iou = 0.45
 show_conf ,show_iou= 0.2, 0.4
 
 # 可以将"image"参数改为gr.webcam()就可以实现摄像头的调用和检测
-# 这里examples参数用来提供两个demo
+# live参数可以使得不按提交按钮可以自动生成检测
+# 利用launch里面的share参数可以实现本地转公网地址
+
 gr.Interface(inputs=["image",gr.Slider(minimum = 0,maximum = 1,value = base_conf),gr.Slider(minimum = 0,maximum = 1,value = base_iou)],
              outputs=["image"],
              fn = det_image,
              title = title,
-             live = True,
              description = desc,
-             examples=[["/home/udf/workspace/wangzijian/yolov5/data/images/bus.jpg",show_conf,show_iou],["/home/udf/workspace/wangzijian/yolov5/data/images/zidane.jpg",base_conf,base_iou]]).launch()
+             live = True,
+             examples=[["/home/udf/workspace/wangzijian/yolov5/data/images/bus.jpg",show_conf,show_iou],["/home/udf/workspace/wangzijian/yolov5/data/images/zidane.jpg",base_conf,base_iou]]).launch(share=True)
